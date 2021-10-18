e7a4ee456e1816c6ba09951591190c0eb855a4c0
VolodumurGruv <gru.v@i.ua>
2021-10-18 22:35:42 +0300

    html template is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { QuestionareComponent } from './questionare/questionare/questionare.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuestionareComponent
-  ],
+  declarations: [AppComponent, QuestionareComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/questionare/questionare/questionare.component.ts
+++ src/app/questionare/questionare/questionare.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-questionare',
   templateUrl: './questionare.component.html',
-  styleUrls: ['./questionare.component.scss']
+  styleUrls: ['./questionare.component.scss'],
 })
 export class QuestionareComponent implements OnInit {
+  stacks = ['angular', 'react', 'vue'];
+  versions = {
+    angular: ['1.1.1', '1.2.1', '1.3.3'],
+    react: ['2.1.2', '3.2.4', '4.3.1'],
+    vue: ['3.3.1', '5.2.1', '5.1.3'],
+  };
 
-  constructor() { }
+  formQuestion = this.fb.group({
+    firstName: [''],
+    sureName: [''],
+  });
 
-  ngOnInit(): void {
-  }
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {}
 }
