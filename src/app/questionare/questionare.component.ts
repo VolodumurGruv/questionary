970acb08432d900ab10872f89af9f38dba756b28
VolodumurGruv <gru.v@i.ua>
2021-10-20 15:13:13 +0300

    before the last changes

--- src/app/questionare/questionare.component.ts
+++ src/app/questionare/questionare.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, LOCALE_ID, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import {
-  EmailValidator,
-  FormArray,
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
 import { EmaileService } from '../services/email.service';
 import { uniqueEmail } from '../validators/email.validator';
+import { PickDateAdapter, _pickFormats } from '../helpres/formatDate';
+import { emailSymbol } from '../validators/email-symbol.validator';
 
 @Component({
   selector: 'app-questionare',
   templateUrl: './questionare.component.html',
   styleUrls: ['./questionare.component.scss'],
-  providers: [EmailValidator],
+  providers: [
+    { provide: DateAdapter, useClass: PickDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: _pickFormats },
+    { provide: MAT_DATE_LOCALE, useValue: 'ru-Ru' },
+    { provide: LOCALE_ID, useValue: 'ru' },
+  ],
 })
 export class QuestionareComponent implements OnInit {
   frameworks = ['angular', 'react', 'vue'];
@@ -21,6 +28,9 @@ export class QuestionareComponent implements OnInit {
     react: ['2.1.2', '3.2.4', '4.3.1'],
     vue: ['3.3.1', '5.2.1', '5.1.3'],
   };
+
+  minDate = new Date(`${new Date().getFullYear() - 100}-01-01`);
+  maxDate = new Date(`${new Date().getFullYear() - 10}-01-01`);
 
   disable: boolean = true;
   vers: string[] = [];
@@ -31,7 +41,7 @@ export class QuestionareComponent implements OnInit {
     email: [
       '',
       {
-        validators: [Validators.required, Validators.email],
+        validators: [Validators.required, Validators.email, emailSymbol()],
         asyncValidators: [uniqueEmail(this.emailService)],
         updateOn: 'blur',
       },
@@ -83,6 +93,7 @@ export class QuestionareComponent implements OnInit {
         duration: ['', Validators.required],
       })
     );
+    console.log(`minDate: ${this.minDate}`);
   }
 
   save() {
